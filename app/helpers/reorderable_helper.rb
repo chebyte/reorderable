9ae57fbbdb9dbdb044fecbe97b875d1fca273c76
mauro <maurotorres@gmail.com>
2011-05-07 16:19:00 -0300

    fixing for empty page model

--- app/helpers/reorderable_helper.rb
+++ app/helpers/reorderable_helper.rb
@@ -1,6 +1,6 @@
 module ReorderableHelper
   def reorderable_attributes(collection, reorder_column='position')
-    if collection
+    unless collection.empty?
       " class='reorderable' data-reorder-url='#{url_for(:controller => "admin/#{collection.first.class.to_s.tableize}", :action => 'reorder', :only_path => true)}' data-reorder-column='#{reorder_column}'"
     end
   end
